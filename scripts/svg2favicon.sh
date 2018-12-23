00cd522f3d10dac05f59e387deaf572e9477757f
Shamil Yakupov <s.yakupov@noviga.com>
2018-12-23 18:00:24 +0300

    Update favicons

--- scripts/svg2favicon.sh
+++ scripts/svg2favicon.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # Prerequisites:
-#     sudo apt install inkscape icoutils
+#     sudo apt install inkscape icoutils optipng
 
 cd "$(dirname "$(readlink -f "${0}")")"
 
 # SVG to PNG
 PNGLIST=''
-for SIZE in 16 32 48
+for SIZE in 16 32
 do
-  inkscape -z ../img/logo/green-dot.svg --export-png=favicon${SIZE}.png --export-width=$SIZE
-  PNGLIST="$PNGLIST favicon${SIZE}.png"
+  inkscape -z ../img/logo/green-dot.svg --export-png=../docs/img/favicon-${SIZE}.png --export-width=$SIZE
+  optipng -o7 ../docs/img/favicon-${SIZE}.png
+  PNGLIST="$PNGLIST ../docs/img/favicon-${SIZE}.png"
 done
 
-
 # PNG to ICO
-icotool -o ../docs/favicon.ico -c $PNGLIST
+icotool -o ../docs/img/favicon.ico -c $PNGLIST
 
-# Clean up
-rm -rf $PNGLIST
+# Various sized PNG
+for SIZE in 48 57 72 114 120 144 152 256 400 512 640
+do
+  inkscape -z ../img/logo/green-dot.svg --export-png=../docs/img/favicon-${SIZE}.png --export-width=$SIZE
+  optipng -o7 ../docs/img/favicon-${SIZE}.png
+done
